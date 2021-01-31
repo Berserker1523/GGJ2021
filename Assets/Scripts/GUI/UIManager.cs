0762cad311e2ab293526536a8400086f647a2c53
KikePerez <ekike797@gmail.com>
2021-01-31 15:02:01 -0500

    Added the main menu scene and logic

---
+++ Assets/Scripts/GUI/UIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject[] screens;
+    public GameObject[] storyScreen;
+
+    private void TurnOffScreens() 
+    {
+        foreach (GameObject go in screens) 
+        {
+            go.SetActive(false);
+        }
+    }
+    private void TurnOffStoryScreens()
+    {
+        foreach (GameObject go in storyScreen)
+        {
+            go.SetActive(false);
+        }
+    }
+    public void ActivateScreen(int index) 
+    {
+        TurnOffScreens();
+        screens[index].SetActive(true);
+    }
+    public void ActivateStoryScreen(int index)
+    {
+        TurnOffStoryScreens();
+        storyScreen[index].SetActive(true);
+    }
+    public void StartGame() 
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}
