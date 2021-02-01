860340912a51e120696d80bd6eb66ce1e2b202ac
Jesús Orlando Cárcamo Posada <berserker1523@hotmail.com>
2021-02-01 14:25:12 -0500

    hud

---
+++ Assets/asd.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class asd : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Main()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
